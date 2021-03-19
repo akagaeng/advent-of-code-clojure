ff4d9150155fb3d0da6d6fb75b994d4df850737f
akagaeng <akagaeng@gmail.com>
2021-03-19 12:06:37 +0900

    Complete part1 : another solution

--- src/_2018/day1.clj
+++ src/_2018/day1.clj
@@ -4,5 +4,8 @@
 (defn readInput [path] (str/split (slurp path) #"\n"))
 
 ; Part 1
+
+; sol1
 (apply + (map read-string (readInput "input/2018/day1.txt")))
+; sol2
 (reduce + (map read-string (readInput "input/2018/day1.txt")))
