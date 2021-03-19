d9e4dea045c89c681098efa51160a9e7c2af4a3e
akagaeng <akagaeng@gmail.com>
2021-03-19 11:58:42 +0900

    Complete part1

--- src/_2018/day1.clj
+++ src/_2018/day1.clj
@@ -1,8 +1,6 @@
 (ns _2018.day1
   (:require [clojure.string :as str]))
 
+(defn readInput [path] (str/split (slurp path) #"\n"))
 
-; read-string
-(str/split
-  (slurp "input/2018/day1.txt")
-  #"\n")
+(apply + (map read-string (readInput "input/2018/day1.txt")))
